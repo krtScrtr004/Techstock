13cbc7ef651b33a7bf5f732aa74c91d4bc87a354
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-17 23:25:53 +0800

    change: make the logo adn the form closer to the center

--- root/frontend/view/login.php
+++ root/frontend/view/login.php
@@ -25,52 +25,56 @@ require_once dirname(__DIR__, 1) . '/utility/definition.php';
         <h1 class="inline">Log In</h1>
     </header>
 
-    <main class="banner flex-row black-bg">
-        <section class="right-pane flex-col center-child">
-            <img src="<?php echo htmlspecialchars(LOGO_PATH . 'logo_complete_hor.svg'); ?>" alt="Techstock Logo" title="Techstock Logo" />
+    <main class="banner flex-row center-child black-bg">
+        <section class="flex-row">
 
-            <h3 class="blue-text">Tech You Want. Stock You Need.</h3>
-        </section>
+            <section class="right-pane flex-col center-child">
+                <img src="<?php echo htmlspecialchars(LOGO_PATH . 'logo_complete_hor.svg'); ?>" alt="Techstock Logo" title="Techstock Logo" />
 
-        <section class="left-pane center-child">
-            <div class="form-wrapper flex-col white-bg">
-                <form action="" method="POST">
-                    <h3 class="black-text">Log In</h3>
+                <h3 class="blue-text">Tech You Want. Stock You Need.</h3>
+            </section>
 
-                    <input type="email" name="l_email" id="l_email" placeholder="Please enter your email here" autocomplete="on" min="3" max="255" required>
+            <section class="left-pane center-child">
+                <div class="form-wrapper flex-col white-bg">
+                    <form action="" method="POST">
+                        <h3 class="black-text">Log In</h3>
 
-                    <div class="password-toggle-wrapper">
-                        <img class="absolute" src="<?php echo htmlspecialchars(ICON_PATH . 'show.svg'); ?>" alt="Show password icon" title="Show password icon" width="18" height="18"/>
+                        <input type="email" name="l_email" id="l_email" placeholder="Please enter your email here" autocomplete="on" min="3" max="255" required>
 
-                        <input type="password" name="l_password" id="l_password" placeholder="Please enter your password here" min="8" max="255" required>
+                        <div class="password-toggle-wrapper">
+                            <img class="absolute" src="<?php echo htmlspecialchars(ICON_PATH . 'show.svg'); ?>" alt="Show password icon" title="Show password icon" width="18" height="18" />
+
+                            <input type="password" name="l_password" id="l_password" placeholder="Please enter your password here" min="8" max="255" required>
+                        </div>
+
+                        <button class="blue-bg white-text" type="submit">LOG IN</button>
+                    </form>
+
+                    <a class="inline black-text" href="#">Forget Password</a>
+
+                    <div class="line-divider relative">
+                        <hr>
+                        <p class="white-bg absolute">OR</p>
                     </div>
 
-                    <button class="blue-bg white-text" type="submit">LOG IN</button>
-                </form>
+                    <section class="oath-provider-link">
+                        <div>
+                            <img src="<?php echo htmlspecialchars(ICON_PATH . 'facebook.svg'); ?>" alt="Facebook Logo" title="Facebook Logo" width="45" height="45">
 
-                <a class="inline black-text" href="#">Forget Password</a>
+                            <h3>Facebook</h3>
+                        </div>
 
-                <div class="line-divider relative">
-                    <hr>
-                    <p class="white-bg absolute">OR</p>
+                        <div>
+                            <img src="<?php echo htmlspecialchars(ICON_PATH . 'google.svg'); ?>" alt="Google Logo" title="Google Logo" width="45" height="45">
+
+                            <h3>Google</h3>
+                        </div>
+                    </section>
+
+                    <p class="block center-text">New to Techstock? <a class="blue-text" href="#">Sign Up</a></p>
                 </div>
+            </section>
 
-                <section class="oath-provider-link">
-                    <div>
-                        <img src="<?php echo htmlspecialchars(ICON_PATH . 'facebook.svg'); ?>" alt="Facebook Logo" title="Facebook Logo" width="45" height="45">
-
-                        <h3>Facebook</h3>
-                    </div>
-
-                    <div>
-                        <img src="<?php echo htmlspecialchars(ICON_PATH . 'google.svg'); ?>" alt="Google Logo" title="Google Logo" width="45" height="45">
-
-                        <h3>Google</h3>
-                    </div>
-                </section>
-
-                <p class="block center-text">New to Techstock? <a class="blue-text" href="#">Sign Up</a></p>
-            </div>
         </section>
     </main>
 
