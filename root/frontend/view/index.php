3f9047629222bb4e4e4a436f78d707051e45e38e
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-18 23:26:49 +0800

    style: add style for set password for sign up page

--- root/frontend/view/index.php
+++ root/frontend/view/index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title>Login</title>
+	<title>Techstock</title>
 
 	<base href="/Techstock/root/frontend/">
 	<link rel="stylesheet" href="<?php echo STYLE_PATH . 'root.css' ?>">
