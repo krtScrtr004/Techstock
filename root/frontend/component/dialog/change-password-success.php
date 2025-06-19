d83681bb7234779a5cb8ec4ad79a5a813772ed9a
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-19 11:43:29 +0800

    feat: create change password success dialog

---
+++ root/frontend/component/dialog/change-password-success.php
@@ -0,0 +1,10 @@
+<section class="modal-wrapper">
+    <div class="dialog">
+        <img src="<?php echo htmlspecialchars(ICON_PATH . 'confirm.svg'); ?>" alt="Confirmation icon" title="Confirmation icon" height="69" width="69">
+
+        <h1 class="center-text">Password Reset</h1>
+        <p class="center-text">Your password was reset successfully.</p>
+
+        <button class="blue-bg white-text">OKAY</button>
+    </div>
+</section>
