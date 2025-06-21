938cfa8f632ebc3721b22327e9b103b669dbd701
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-21 15:20:50 +0800

    change: make header component file dynamic by adding cases whether session is set or not to determine which headr to display

--- root/frontend/component/header.php
+++ root/frontend/component/header.php
@@ -1,3 +1,5 @@
+<? if (session_status() === PHP_SESSION_NONE): ?>
+<!-- Header for non-logged in pages -->
 <header class="outside-header flex-row flex-child-end-h">
     <a href="<?php echo htmlspecialchars(REDIRECT_PATH . 'login/'); ?> ">
         <img src="<?php echo htmlspecialchars(LOGO_PATH . 'logo_complete_ver.svg'); ?>" alt="Techstock Logo" title="Techstock Logo" height="45" />
@@ -5,3 +7,10 @@
 
     <h3><?php echo ($page === 'signup') ? "Sign Up" : "Log In"; ?></h3>
 </header>
+
+<? elseif (session_status() === PHP_SESSION_ACTIVE): ?>
+<!-- Header for logged in pages -->
+
+<? else: ?>
+
+<? endif ?>
