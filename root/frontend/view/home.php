1a4da21f7163feb2017f630465851f537cddadb6
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-21 22:38:24 +0800

    style: style homepage slideshow

--- root/frontend/view/home.php
+++ root/frontend/view/home.php
@@ -1,19 +1,51 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <title>Techstock | Home</title>
 
-	<base href="/Techstock/root/frontend/">
-	<link rel="stylesheet" href="<?php echo STYLE_PATH . 'root.css' ?>">
-	<link rel="stylesheet" href="<?php echo STYLE_PATH . 'utility.css' ?>">
-	<link rel="stylesheet" href="<?php echo STYLE_PATH . 'component.css' ?>">
-	<link rel="stylesheet" href="<?php echo STYLE_PATH . 'header.css' ?>">
+    <base href="/Techstock/root/frontend/">
+    <link rel="stylesheet" href="<?php echo STYLE_PATH . 'root.css' ?>">
+    <link rel="stylesheet" href="<?php echo STYLE_PATH . 'utility.css' ?>">
+    <link rel="stylesheet" href="<?php echo STYLE_PATH . 'component.css' ?>">
+    <link rel="stylesheet" href="<?php echo STYLE_PATH . 'header.css' ?>">
 
+    <link rel="stylesheet" href="<?php echo STYLE_PATH . 'home.css' ?>">
 </head>
-<body>
-    This is Home.
+
+<body class="home flex-col">
+    <?php include_once COMPONENT_PATH . 'header.php'; ?>
+
+    <main class="dark-white-bg">
+        <section class="banner-section white-bg">
+            <div class="slide-show-wrapper relative">
+                <img class="slide-show" src="<?php echo htmlspecialchars(IMAGE_PATH . 'console-1.jpg'); ?>" alt="Slideshow" title="Slideshow" height="300" loading="lazy">
+
+                <div class="change-button previous circle center-child absolute white-bg">
+                    <img src="<?php echo htmlspecialchars(ICON_PATH . 'next.svg'); ?>" alt="Previous button" title="Previous button" height="20" width="20">
+                </div>
+
+                <div class="change-button next circle center-child absolute white-bg">
+                    <img src="<?php echo htmlspecialchars(ICON_PATH . 'next.svg'); ?>" alt="Next button" title="Next button" height="20" width="20">
+                </div>
+
+                <span class="absolute flex-row">
+                    <div class="dot-tab active circle white-bg"></div>
+                    <div class="dot-tab circle white-bg"></div>
+                    <div class="dot-tab circle white-bg"></div>
+                    <div class="dot-tab circle white-bg"></div>
+                    <div class="dot-tab circle white-bg"></div>
+                </span>
+            </div>
+
+
+        </section>
+    </main>
 </body>
+
 </html>
