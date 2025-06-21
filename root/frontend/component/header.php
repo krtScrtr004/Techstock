2438a3272286aa64e1010f7ddaf31790ac8db854
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-21 16:18:21 +0800

    style: add style on links on upper part of the inside header

--- root/frontend/component/header.php
+++ root/frontend/component/header.php
@@ -1,4 +1,4 @@
-<? if (session_status() === PHP_SESSION_NONE): ?>
+<?php if (session_status() === PHP_SESSION_NONE): ?>
 <!-- Header for non-logged in pages -->
 <header class="outside-header flex-row flex-child-end-h">
     <a href="<?php echo htmlspecialchars(REDIRECT_PATH . 'login/'); ?> ">
@@ -8,9 +8,46 @@
     <h3><?php echo ($page === 'signup') ? "Sign Up" : "Log In"; ?></h3>
 </header>
 
-<? elseif (session_status() === PHP_SESSION_ACTIVE): ?>
+<?php elseif (session_status() === PHP_SESSION_ACTIVE): ?>
 <!-- Header for logged in pages -->
+<header class="inside-header black-bg">
 
-<? else: ?>
+    <section class="top-link-section flex-row white-text">
+        <section class="left-side flex-row">
+            <a class="white-text" href="#">Start Selling</a>
 
-<? endif ?>
+            <span class="link-w-icon flex-row">
+                <p>Follow us on</p>
+                <a href="#">
+                    <img src="<?php echo htmlspecialchars(ICON_PATH . 'facebook_w.svg'); ?>" alt="Facebook Icon" title="Facebook Icon" height="16" width="16">
+                </a>
+                <a href="#">
+                    <img src="<?php echo htmlspecialchars(ICON_PATH . 'instagram_w.svg'); ?>" alt="Instagram Icon" title="Instagram Icon" height="16" width="16">
+                </a>
+                <a href="#">
+                    <img src="<?php echo htmlspecialchars(ICON_PATH . 'tiktok_w.svg'); ?>" alt="Tiktok Icon" title="Tiktok Icon" height="16" width="16">
+                </a>
+            </span>
+        </section>
+
+        <section class="right-side flex-row flex-child-end-v">
+            <span class="link-w-icon flex-row">
+                <img src="<?php echo htmlspecialchars(ICON_PATH . 'notification.svg'); ?>" alt="Bell icon" title="Bell icon" height="16" width="16">
+                <p>Notification</p>
+            </span>
+
+            <span class="link-w-icon flex-row">
+                <img src="<?php echo htmlspecialchars(ICON_PATH . 'help.svg'); ?>" alt="Question mark icon" title="Question mark icon" height="16" width="16">
+                <p>Help</p>
+            </span>
+
+            <span class="link-w-icon flex-row">
+                <img src="<?php echo htmlspecialchars(ICON_PATH . 'user-profile.svg'); ?>" alt="User profile icon" title="User profile icon" height="16" width="16">
+                <p>Username-123</p>
+            </span>
+        </section>
+    </section>
+</header>
+<?php else: ?>
+
+<?php endif ?>
