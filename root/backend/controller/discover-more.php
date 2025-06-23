e55b39bb850e64810a78639646a7bb6dd28f2567
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-23 22:36:17 +0800

    feat: add discover more page

---
+++ root/backend/controller/discover-more.php
@@ -0,0 +1,27 @@
+<?php
+function discoverMore(): void {
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+    
+    $products = [
+        new Product(
+            123, 
+            'Product 1',
+            'This is product 1.',
+            18.99,
+            123
+        ),
+        new Product(
+            321, 
+            'Product 2',
+            'This is product 2.',
+            99.23,
+            321
+        )
+    ];
+
+    require_once VIEW_PATH . 'discover-more.php';
+
+    errorOccuredDialog();
+}
--- root/frontend/view/home.php
+++ root/frontend/view/home.php
@@ -169,28 +169,10 @@
         </section>
 
         <!-- Discover More -->
-        <section class="discover-more-section ">
+        <section class="discover-more-section">
             <h3 class="home-section-heading center-text">Discover More</h3>
 
-            <section class="product-list">
-                <?php
-                foreach ($products as $product) {
-                    renderProductCard($product);
-                } ?>
-            </section>
-
-            <div class="page-tab center-child">
-                <img class="previous center-child" src="<?php echo htmlspecialchars(ICON_PATH . 'next.svg') ?>" alt="Previous button" title="Previous button" height="24">
-
-                <p class="active">1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
-                <p>...</p>
-
-                <img class="next center-child" src="<?php echo htmlspecialchars(ICON_PATH . 'next.svg') ?>" alt="Next button" title="Next button" height="24">
-            </div>
+            <?php include_once COMPONENT_PATH . 'product-list.php' ?>
         </section>
 
         <section class="article-list flex-col white-bg">
