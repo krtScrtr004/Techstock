b773273eb5bc6a72a389d620bcf5ed9d218f6b47
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-25 11:03:33 +0800

    change: make product card's alt aand title the product's name

--- root/frontend/utility/render-product-card.php
+++ root/frontend/utility/render-product-card.php
@@ -5,7 +5,7 @@ function renderProductCard(Product $product): void
     <a href="">
         <div class="product-card">
             <!-- TODO: Change this to product's url -->
-            <img src="<?php echo htmlspecialchars(IMAGE_PATH . 'laptop-1.jpg') ?>" alt="Product image" title="Product image" height="200">
+            <img src="<?php echo htmlspecialchars(IMAGE_PATH . 'laptop-1.jpg') ?>" alt="<?php echo htmlspecialchars($product->getName()) ?>" title="<?php echo htmlspecialchars($product->getName()) ?>" height="200">
 
             <h3 class="multi-line-ellipsis">
                 <?php echo htmlspecialchars($product->getDescription()); ?>
@@ -13,7 +13,7 @@ function renderProductCard(Product $product): void
 
             <span>
                 <p>
-                    <?php echo $product->getCurrency() . $product->getPrice()?>
+                    <?php echo $product->getCurrency() . htmlspecialchars($product->getPrice())?>
                 </p>
                 <p>1.3k Sold</p>
             </span>
