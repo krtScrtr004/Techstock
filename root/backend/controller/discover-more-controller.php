05b9274e9c469354926a547aa3a8fa107bebe1f0
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-27 21:51:52 +0800

    change: make desciver more a controller class

--- root/backend/controller/discover-more-controller.php
+++ root/backend/controller/discover-more-controller.php
@@ -1,19 +1,18 @@
 <?php
 
-class HomeController implements Controller
+class DiscoverMoreController implements Controller
 {
     private function __construct() {}
-
+    
     public static function index(): void
     {
         global $session;
-        if (!isset($session))
-            $session = Session::create();
+        if (!isset($session)) $session = Session::create();
 
         $productModel = new Product();
 
         $products = $productModel->all();
-        require_once VIEW_PATH . 'home.php';
+        require_once VIEW_PATH . 'discover-more.php';
 
         errorOccuredDialog();
     }
