781896541e3336c0d0dada988a89077259d9a02f
krtScrtr44 <secretariokurt3219@gmail.com>
2025-06-27 22:01:02 +0800

    change: make search a controller class

---
+++ root/backend/controller/search-controller.php
@@ -0,0 +1,33 @@
+<?php
+
+enum Category: string
+{
+    case sna    = 'smartphone-and-accessories';
+    case cnl    = 'computers-and-laptops';
+    case cnpp   = 'components-and-pc-parts';
+    case gm     = 'gaming';
+    case nnsh   = 'networking-and-smart-home';
+    case anm    = 'audio-and-music';
+    case wnht   = 'wearable-and-health-tech';
+    case onp    = 'office-and-productivity';
+    case dnc    = 'drone-and-cameras';
+    case tfe    = 'tech-for-education';
+};
+
+class SearchController implements Controller
+{
+    private function __construct() {}
+
+    public static function index(): void
+    {
+        global $session;
+        if (!isset($session)) $session = Session::create();
+
+        $productModel = new Product();
+        $products = $productModel->all();
+
+        require_once VIEW_PATH . 'search.php';
+
+        errorOccuredDialog();
+    }
+}
