dc0f845c9e761102767be83d23df1a3f5d4ee551
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-02 23:37:48 +0800

    controller: create controller for page view info page

---
+++ root/backend/controller/product-controller.php
@@ -0,0 +1,22 @@
+<?php
+
+class ProductController implements Controller 
+{
+    private function __construct() {}
+
+    public static function index(): void {}
+
+    public static function info(): void 
+    {
+        global $session;
+        if (!isset($session)) $session = Session::create();
+
+        // Dummy
+        $products = Product::all();
+        $product = $products[0];
+
+        require_once VIEW_PATH . 'product-info.php';
+
+        errorOccurredDialog();
+    }
+}
