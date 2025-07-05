5a66df047802c6f6f19695ab263d44611de7e5a1
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-05 15:32:27 +0800

    script: add script to copy link of product

---
+++ root/frontend/script/event/copy-link.js
@@ -0,0 +1,42 @@
+function animateNotification(notificationWrapper) {
+    const notificationContent = notificationWrapper.querySelector('div');
+    const duration = notificationContent.getAttribute('data-duration') ?? 5000;
+
+    // Define the handler outside the setTimeout to prevent undefined behaviors 
+    // When copy-link button is clicked multiple successively
+    function onAnimationEnd(ev) {
+        if (ev.animationName === 'slide-up') {
+            notificationContent.removeEventListener('animationend', onAnimationEnd);
+            notificationWrapper.style.display = 'none';
+        }
+    }
+
+    // Clear previous animation classes
+    notificationContent.classList.remove('slide-up');
+
+    notificationWrapper.style.display = 'block';
+    notificationContent.classList.add('slide-down');
+
+    setTimeout(() => {
+        notificationContent.classList.remove('slide-down');
+        notificationContent.classList.add('slide-up');
+
+        // Remove existing listeners (optional safety) and add again
+        notificationContent.removeEventListener('animationend', onAnimationEnd);
+        notificationContent.addEventListener('animationend', onAnimationEnd);
+    }, duration);
+}
+
+
+const copyLink = document.querySelector('.copy-link')
+copyLink.addEventListener('click', e => {
+    e.preventDefault()
+    navigator.clipboard.writeText(window.location.href)
+        .then(() => {
+            animateNotification(document.querySelector('.notification-wrapper.success'))
+        })
+        .catch(() => {
+            animateNotification(document.querySelector('.notification-wrapper.fail'))
+        })
+})
+
