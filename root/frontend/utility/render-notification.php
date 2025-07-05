b386ac3005ed4ffa2d3a39426ef6eded5cb9fdc2
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-05 14:20:40 +0800

    func: add a function that renders a notification

---
+++ root/frontend/utility/render-notification.php
@@ -0,0 +1,11 @@
+<?php
+
+function renderNotification(): void {
+?>
+    <section class="notification-wrapper center-child absolute">
+        <div class="blue-bg">
+            <p class="white-text">This is a notification.</p>
+        </div>
+    </section>
+<?php
+}
