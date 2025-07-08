9d39344cc684628db4e549134111a1e21e174e76
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-08 16:20:49 +0800

    model: add utility model - rating-reply - for product rating model

---
+++ root/backend/model/rating-reply.php
@@ -0,0 +1,47 @@
+<?php
+
+class RatingReply
+{
+    private $id;
+    private string $reply;
+    private DateTime $createdAt;
+
+    public function __construct(array $data = [])
+    {
+        $this->$data['id'];
+        $this->reply =  $data['reply'];
+        $this->createdAt = $data['created_at'] ?? new DateTime();
+    }
+
+    // Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getReply(): string
+    {
+        return $this->reply;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    // Setters
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setReply(string $reply): void
+    {
+        $this->reply = $reply;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {            
+        $this->createdAt = $createdAt;
+    }
+}
