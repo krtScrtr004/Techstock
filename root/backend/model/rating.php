00b2a6cdc3ab96b32e2156e10c5092ee2b714396
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-08 20:52:05 +0800

    model: add rater property and methods for rating model

--- root/backend/model/rating.php
+++ root/backend/model/rating.php
@@ -1,16 +1,19 @@
 <?php
 
-class Rating implements Model 
+class Rating implements Model
 {
     private $id;
+    private User $rater;
     private int $rating;
     private ?string $comment;
     private int $like;
     private DateTime $createdAt;
     private ?RatingReply $reply;
 
-    public function __construct(array $data = []) {
+    public function __construct(array $data = [])
+    {
         $this->id = $data['id'];
+        $this->rater = $data['rater'];
         $this->rating = $data['rating'] ?? 0;
         $this->comment = $data['comment'] ?? null;
         $this->like = $data['like'] ?? 0;
@@ -19,89 +22,110 @@ class Rating implements Model
     }
 
     // Getters
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getRating(): int {
+    public function getRater(): User
+    {
+        return $this->rater;
+    }
+
+    public function getRating(): int
+    {
         return $this->rating;
     }
 
-    public function getComment(): ?string {
+    public function getComment(): ?string
+    {
         return $this->comment;
     }
 
-    public function getLike(): int {
+    public function getLike(): int
+    {
         return $this->like;
     }
 
-    public function getCreatedAt(): DateTime {
+    public function getCreatedAt(): DateTime
+    {
         return $this->createdAt;
     }
 
-    public function getReply(): ?RatingReply {
+    public function getReply(): ?RatingReply
+    {
         return $this->reply;
     }
 
     // Setters
-    public function setId($id): void {
+    public function setId($id): void
+    {
         $this->id = $id;
     }
 
-    public function setRating(int $rating): void {
+    public function setRater($rater): void
+    {
+        $this->rater = $rater;
+    }
+
+    public function setRating(int $rating): void
+    {
         $this->rating = $rating;
     }
 
-    public function setComment(?string $comment): void {
+    public function setComment(?string $comment): void
+    {
         $this->comment = $comment;
     }
 
-    public function setLike(int $like): void {
+    public function setLike(int $like): void
+    {
         $this->like = $like;
     }
 
-    public function setCreatedAt(DateTime $createdAt): void {
+    public function setCreatedAt(DateTime $createdAt): void
+    {
         $this->createdAt = $createdAt;
     }
 
-    public function setReply(?RatingReply $reply): void {
+    public function setReply(?RatingReply $reply): void
+    {
         $this->reply = $reply;
     }
 
     // Implemented methods
-	public static function all(): array
-	{
-		// TODO: Implement all() method.
-		return [];
-	}
+    public static function all(): array
+    {
+        // TODO: Implement all() method.
+        return [];
+    }
 
-	public static function create(array $data): Model
-	{
-		// TODO: Implement create() method.
-		return new self();
-	}
+    public static function create(array $data): Model
+    {
+        // TODO: Implement create() method.
+        return new self();
+    }
 
-	public function delete(): bool
-	{
-		// TODO: Implement delete() method.
-		return true;
-	}
+    public function delete(): bool
+    {
+        // TODO: Implement delete() method.
+        return true;
+    }
 
-	public function fill(array $data): void
-	{
-		// TODO: Implement fill() method.
-	}
+    public function fill(array $data): void
+    {
+        // TODO: Implement fill() method.
+    }
 
-	public static function find($id): ?Model
-	{
-		// TODO: Implement find() method.
-		return null;
-	}
+    public static function find($id): ?Model
+    {
+        // TODO: Implement find() method.
+        return null;
+    }
 
-	public function save(): bool
-	{
-		// TODO: Implement save() method.
-		return true;
-	}
+    public function save(): bool
+    {
+        // TODO: Implement save() method.
+        return true;
+    }
 }
-
