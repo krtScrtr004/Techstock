fed87acdb0d0c5c37f505149454917a01c1ff141
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-09 23:47:42 +0800

    script: create a factory function for http method request in fe

---
+++ root/frontend/script/utility/http.js
@@ -0,0 +1,25 @@
+const Http = () => {
+    const apiUrl = 'http://localhost/Techstock/'
+
+    return {
+        GET: async (endpoint) => {
+            try {
+                const request = await fetch(`${apiUrl}${endpoint}`)
+                if (!request.ok) {
+                    throw new Error(`HTTP error! Status: ${request.status} ${request.statusText}`);
+                }
+
+                const contentType = request.headers.get('content-type');
+                if (!contentType || !contentType.includes('application/json')) {
+                    throw new Error('Expected JSON, but got non-JSON response');
+                }
+
+                return await request.json()
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+    }
+}
+export const http = Http()
