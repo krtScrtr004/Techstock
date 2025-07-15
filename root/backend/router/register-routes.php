d9df585c816a7b709bb8bbdefeedda05ac202e69
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-15 22:33:39 +0800

    change: rename routes.jon to page-routes.json

--- root/backend/router/register-routes.php
+++ root/backend/router/register-routes.php
@@ -1,6 +1,6 @@
 <?php
 
-$fileNames = ['routes.json', 'form-submission-routes.json'];
+$fileNames = ['page-routes.json', 'form-submission-routes.json'];
 
 foreach ($fileNames as $fileName) {
     $routesFile = DATA_PATH . $fileName;
