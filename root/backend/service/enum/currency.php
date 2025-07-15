ccca3f37e3ad69a69bc15ff4b8785b1c312108e8
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-15 15:39:05 +0800

    enum: create a currency enum

---
+++ root/backend/service/enum/currency.php
@@ -0,0 +1,72 @@
+<?php
+
+enum Currency: string
+{
+    case UnitedStates = 'USD';
+    case Philippines = 'PHP';
+    case Eurozone = 'EUR';
+    case UnitedKingdom = 'GBP';
+    case Japan = 'JPY';
+    case China = 'CNY';
+    case SouthKorea = 'KRW';
+    case Australia = 'AUD';
+    case Canada = 'CAD';
+    case India = 'INR';
+    case Singapore = 'SGD';
+    case Thailand = 'THB';
+    case Malaysia = 'MYR';
+    case Indonesia = 'IDR';
+    case Vietnam = 'VND';
+
+    public function symbol(): string
+    {
+        return match ($this) {
+            self::UnitedStates => '$',
+            self::Philippines => '₱',
+            self::Eurozone => '€',
+            self::UnitedKingdom => '£',
+            self::Japan => '¥',
+            self::China => '¥',
+            self::SouthKorea => '₩',
+            self::Australia => 'A$',
+            self::Canada => 'C$',
+            self::India => '₹',
+            self::Singapore => 'S$',
+            self::Thailand => '฿',
+            self::Malaysia => 'RM',
+            self::Indonesia => 'Rp',
+            self::Vietnam => '₫',
+        };
+    }
+
+    public static function fromCountry(string $country): ?self
+    {
+        return match (ucwords(strtolower($country))) {
+            'United States' => self::UnitedStates,
+            'Eurozone' => self::Eurozone,
+            'United Kingdom' => self::UnitedKingdom,
+            'Japan' => self::Japan,
+            'China' => self::China,
+            'South Korea' => self::SouthKorea,
+            'Australia' => self::Australia,
+            'Canada' => self::Canada,
+            'India' => self::India,
+            'Singapore' => self::Singapore,
+            'Thailand' => self::Thailand,
+            'Malaysia' => self::Malaysia,
+            'Indonesia' => self::Indonesia,
+            'Vietnam' => self::Vietnam,
+            default => self::Philippines,
+        };
+    }
+
+    public static function tryFromName(string $name): ?self
+    {
+        foreach (self::cases() as $case) {
+            if ($case->name === $name) {
+                return $case;
+            }
+        }
+        return null;
+    }
+}
