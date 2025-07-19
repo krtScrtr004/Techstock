c10d4802485d59ba2e1ab12442a2d2b2ef8d6c06
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-20 00:31:20 +0800

    change: add variation of loader (full page/section and trail)

--- root/frontend/script/render/loader.js
+++ root/frontend/script/render/loader.js
@@ -1,24 +1,43 @@
 const Loader = () => {
     let parent;
 
+    function render(parentElem, loaderHtml, position) {
+        if (!parentElem) {
+            throw new Error('No parent element');
+        }
+        parentElem.style.position = 'relative';
+        parentElem.insertAdjacentHTML(position, loaderHtml);
+        parent = parentElem;
+    }
+
     return {
-        create: function (parentElem) {
-            if (!parentElem) {
-                throw new Error('No parent element')
-            }
+        full: function (parentElem) {
+            const parentHeight = parentElem.offsetHeight;
+            const loaderElem = `
+                <div 
+                    class="loader-wrapper center-child absolute white-bg" 
+                    style="height:${parentHeight}; top:0; left:0; right:0; bottom:0;">
+                        <span class="loader"></span>
+                </div>`;
+            render(parentElem, loaderElem, 'afterbegin');
+        },
 
-            const loaderElem = '<div class="loader-wrapper center-child full-body-content"><span class="loader"></span></div>'
-            parentElem.insertAdjacentHTML('afterbegin', loaderElem)
-
-            this.parent = parentElem
+        trail: function (parentElem) {
+            const loaderElem = `
+                <div 
+                    class="loader-wrapper center-child white-bg absolute" 
+                    style="height:fit-content; bottom:0;">
+                        <span class="loader"></span>
+                </div>`;
+            render(parentElem, loaderElem, 'beforeend');
         },
 
         delete: function () {
-            const createdLoader = this.parent.querySelector('.loader-wrapper')
+            const createdLoader = parent?.querySelector('.loader-wrapper');
             if (createdLoader) {
                 createdLoader.remove();
             }
         }
     }
 }
-export const loader = Loader()
+export const loader = Loader();
