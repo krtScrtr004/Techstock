ae7bf39bf319b217afb38b18530f786950233cb0
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-21 00:52:17 +0800

    change: migrate Dialogs from server side rendered to client side

---
+++ root/frontend/script/render/dialog.js
@@ -0,0 +1,73 @@
+const Dialog = () => {
+    const ICON_PATH = 'asset/image/icon/'
+
+    function render(
+        status,
+        id,
+        title,
+        message,
+        parent = document.querySelector('body')
+    ) {
+        const icons = ['confirm.svg', 'reject.svg']
+        const statusIcon = status ? icons[0] : icons[1]
+
+        const html = `
+        <section id="${id}-wrapper" class="modal-wrapper">
+            <section class="dialog white-bg">
+                <img 
+                    src="${ICON_PATH + statusIcon}" 
+                    alt="Result icon" 
+                    title="Result icon" 
+                    height="69" 
+                    width="69" />
+
+                <h1 class="center-text">${title}</h1>
+                <p class="center-text">${message}</p>
+
+                <button class="okay-button ${status ? 'blue-bg' : 'red-bg'} white-text">OKAY</button>
+            </section>
+        </section>
+        `
+
+        parent.insertAdjacentHTML('afterbegin', html)
+    }
+
+    return {
+        errorOccurred: function (message) {
+            render(
+                false,
+                'error-occurred-dialog',
+                'Error Occurred',
+                message
+            )
+        },
+
+        changePassword: function (status) {
+            if (status) {
+                render(
+                    status,
+                    'change-password-success-dialog',
+                    'Password Reset',
+                    'Your password was changed successfully.'
+                )
+            } else {
+                render(
+                    status,
+                    'change-password-error-dialog',
+                    'Password Reset',
+                    'There was a problem changing your password. Please try again.'
+                )
+            }
+        },
+
+        tooManyAttempt: function () {
+            render(
+                false,
+                'too-many-attempt-dialog',
+                'Too Many Attempt',
+                'Access temporarily locked due to multiple failed attempts. Try again in 2 minutes.'
+            )
+        }
+    }
+}
+export const dialog = Dialog()
