9e04113cdb68865b452721d20a70e51cc58aeab3
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-26 22:48:03 +0800

    controller: create controller for shopping cart page

---
+++ root/backend/controller/shopping-cart-controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class ShoppingCartController implements Controller
+{
+    public static function index(): void
+    {
+        global $session;
+
+        // require_once VIEW_PATH . 'shopping-cart.php';
+    }
+}
