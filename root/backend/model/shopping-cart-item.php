c1833930890052b425462c190508ce4cfdb1ead6
krtScrtr44 <secretariokurt3219@gmail.com>
2025-07-28 00:20:21 +0800

    model: add selected options properties and methods for shopping cart item model

--- root/backend/model/shopping-cart-item.php
+++ root/backend/model/shopping-cart-item.php
@@ -4,10 +4,13 @@ class ShoppingCartItem extends Product
 {
     private int $quantity;
 
+    private ?ProductOption $selectedOptions;
+
     public function __construct(array $data) 
     {
         parent::__construct($data);
         $this->quantity = $data['quantity'] ?? 0;
+        $this->selectedOptions = $data['selected_options'] ?? null;
     }
 
     // Getters
@@ -17,11 +20,21 @@ class ShoppingCartItem extends Product
         return $this->quantity;
     }
 
+    public function getSelectedOptions(): ?ProductOption
+    {
+        return $this->selectedOptions;
+    }
+
     // Setters
 
     public function setQuantity(int $quantity): void
     {
         $this->quantity = $quantity;
+    }
+
+    public function setSelectedOptions(ProductOption $options): void
+    {
+        $this->selectedOptions = $options;
     }
 
     // Utilities
