514eef8ae93ff3a2fce222dca18d7a250cdad80b
krtScrtr44 <secretariokurt3219@gmail.com>
2025-08-01 15:01:34 +0800

    style: add form for writing a message

--- source/frontend/component/chat.php
+++ source/frontend/component/chat.php
@@ -50,7 +50,7 @@
 
         <section class="chat-content flex-col flex-space-between flex-child-center-v">
             <!-- Heading -->
-            <section class="chat-content-heading flex-row white-bg">
+            <section class="chat-content-heading flex-row flex-child-center-h white-bg">
                 <img
                     class="store-logo circle fit-contain white-bg"
                     src="<?= IMAGE_PATH . 'brand logo/acer.png' ?>"
@@ -64,12 +64,14 @@
                 </div>
 
                 <div class="more-options flex-row-reverse flex-child-center-h relative">
-                    <img
-                        class="circle"
-                        src="<?= ICON_PATH . 'more_b.svg' ?>"
-                        alt="More options"
-                        title="More options"
-                        height="30" />
+                    <button class="unset-button">
+                        <img
+                            class="circle"
+                            src="<?= ICON_PATH . 'more_b.svg' ?>"
+                            alt="More options"
+                            title="More options"
+                            height="30" />
+                    </button>
 
                     <!-- Dropdown -->
                     <section class="dropdown white-bg absolute no-display">
@@ -95,9 +97,70 @@
                 </div>
             </section>
 
+            <!-- Main Content -->
+            <section class="chat-content-main flex-col dark-white-bg">
+                <!-- Input Area -->
+                <section class="write-message-area white-bg">
+                    <form action="">
+                        <section>
+                            <input
+                                class="written-message-content transparent-bg"
+                                type="text"
+                                id="written_message_content"
+                                name="written_message_content"
+                                placeholder="Type a message here..."
+                                min="1"
+                                max="500" />
+                        </section>
 
-            <section class="chat-content-main dark-white-bg">
+                        <section class="flex-row flex-space-between transparent-bg">
+                            <section class=" flex-row media-picker">
+                                <!-- Image Picker -->
+                                <div class="image-picker">
+                                    <input
+                                        class="no-display"
+                                        type="file"
+                                        name="image_upload"
+                                        id="image_upload"
+                                        accept="image/*" />
+                                    <button class="open-file-picker-button unset-button" type="button">
+                                        <img
+                                            src="<?= ICON_PATH . 'image_b.svg' ?>"
+                                            alt="Include an image"
+                                            title="Include an image"
+                                            height="32" />
+                                    </button>
+                                </div>
 
+                                <!-- Video Picker -->
+                                <div class="video-picker">
+                                    <input
+                                        class="no-display"
+                                        type="file"
+                                        name="video_upload"
+                                        id="video_upload"
+                                        accept="video/*" />
+                                    <button class="open-file-picker-button unset-button" type="button">
+                                        <img
+                                            src="<?= ICON_PATH . 'video_b.svg' ?>"
+                                            alt="Include a video"
+                                            title="Include a video"
+                                            height="32" />
+                                    </button>
+                                </div>
+                            </section>
+
+                            <button class="send-message-button unset-button" type="button">
+                                <img
+                                    src="<?= ICON_PATH . 'send_bl.svg' ?>"
+                                    alt="Send"
+                                    title="Send"
+                                    height="32" />
+                            </button>
+                        </section>
+
+                    </form>
+                </section>
             </section>
         </section>
     </section>
