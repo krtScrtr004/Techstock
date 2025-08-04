4b00d6531cd208aa85269d7539d463d1abd0cb42
krtScrtr44 <secretariokurt3219@gmail.com>
2025-08-04 08:42:20 +0800

    change: move insertFragment function to displayBatch file

--- public/script/utility/display-batch.js
+++ public/script/utility/display-batch.js
@@ -1,4 +1,24 @@
-import { insertFragment } from './insert-fragment.js'
+function insertFragment(htmlArray, parentElem, prepend = false) {
+    if (!htmlArray) {
+        throw new Error('No html array defined')
+    }
+
+    if (!parentElem) {
+        throw new Error('No parent element defined')
+    }
+
+    const fragment = document.createDocumentFragment()
+    htmlArray.forEach(html => {
+        const node = document.createRange().createContextualFragment(html)
+        fragment.appendChild(node)
+    })
+    
+    if (prepend) {
+        parentElem.insertBefore(fragment, parentElem.firstChild)
+    } else {
+        parentElem.appendChild(fragment)
+    }
+}
 
 export function displayBatch(parentElem, prepend = false) {
     if (!parentElem) {
